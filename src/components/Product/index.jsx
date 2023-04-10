6d82355f4a9d855a6743bfa98d82397c540dc714
qori balqis <qoribalqis37@gmail.com>
2023-04-10 13:42:33 +0700

    add alert notify

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -16,72 +16,80 @@ const Product = ({ onDelete, changeWishList }) => {
         <h5 className="mb-4 text-xl font-bold tracking-tighter text-gray-900 w-dark:text-white">
           Cart {`(${totalCart} items)`}
         </h5>
-        {productList.map((item, index) => (
-          <Fragment key={index}>
-            <div className="py-4 flex w-full m876:flex-col">
-              <div className="md:flex md:justify-between s400:items-end flex min-w-fit">
-                <div
-                  className="block p-6 max-w-fit min-w-fit bg-gray-300 rounded-lg border
+        {productList.length === 0 ? (
+          <div>Data Product Empty...</div>
+        ) : (
+          productList.map((item, index) => (
+            <Fragment key={index}>
+              <div className="py-4 flex w-full m876:flex-col">
+                <div className="md:flex md:justify-between s400:items-end flex min-w-fit">
+                  <div
+                    className="block p-6 max-w-fit min-w-fit bg-gray-300 rounded-lg border
                     border-gray-200"
-                >
-                  <img className="w-24 h-24" src={item.img} alt="jaket-hitam" />
-                </div>
-                <Counter
-                  displayMobile="sm:display  mdin:hidden s400:h-28 h-fit  s400:flex-col "
-                  id={item.productId}
-                  numberItem={item.numOfItem}
-                />
-              </div>
-              <div className="ml-6 m876:ml-0 md:ml-0 sm:ml-0 flex flex-wrap">
-                <div className="flex w-full h-fit justify-between ">
-                  <p className=" text-xl font-bold">{item.title}</p>
+                  >
+                    <img
+                      className="w-24 h-24"
+                      src={item.img}
+                      alt="jaket-hitam"
+                    />
+                  </div>
                   <Counter
-                    displayMobile="md:hidden"
+                    displayMobile="sm:display  mdin:hidden s400:h-28 h-fit  s400:flex-col "
                     id={item.productId}
                     numberItem={item.numOfItem}
                   />
                 </div>
-                <div className="flex w-full justify-between text-sm ">
-                  <p>{item.subTitle}</p>
-                  <p className="">{item.note}</p>
-                </div>
-                <div className="flex w-full text-sm mt-4">
-                  <p className="">COLOR : {item.color}</p>
-                </div>
-                <div className="flex w-full text-sm mt-2 ">
-                  <p className="">SIZE - {item.size}</p>
-                </div>
-                <div className="flex w-full sm:flex-wrap-reverse justify-between text-sm mt-4 ">
-                  <div className=" flex">
-                    <div
-                      onClick={() => onDelete(item.productId)}
-                      className="flex cursor-pointer hover:text-red-500"
-                    >
-                      <BiTrash size={20} />
-                      <span className="ml-2">REMOVE ITEM</span>
-                    </div>
-                    <div
-                      onClick={() => changeWishList(item.productId)}
-                      className={
-                        item?.like
-                          ? "ml-6 flex cursor-pointer text-red-500 "
-                          : "ml-6 flex cursor-pointer"
-                      }
-                    >
-                      <BsFillSuitHeartFill size={20} />
-                      <span className="ml-2">MOVE TO WISH LIST</span>
-                    </div>
+                <div className="ml-6 m876:ml-0 md:ml-0 sm:ml-0 flex flex-wrap">
+                  <div className="flex w-full h-fit justify-between ">
+                    <p className=" text-xl font-bold">{item.title}</p>
+                    <Counter
+                      displayMobile="md:hidden"
+                      id={item.productId}
+                      numberItem={item.numOfItem}
+                    />
                   </div>
+                  <div className="flex w-full justify-between text-sm ">
+                    <p>{item.subTitle}</p>
+                    <p className="">{item.note}</p>
+                  </div>
+                  <div className="flex w-full text-sm mt-4">
+                    <p className="">COLOR : {item.color}</p>
+                  </div>
+                  <div className="flex w-full text-sm mt-2 ">
+                    <p className="">SIZE - {item.size}</p>
+                  </div>
+                  <div className="flex w-full sm:flex-wrap-reverse justify-between text-sm mt-4 ">
+                    <div className=" flex">
+                      <div
+                        onClick={() => onDelete(item.productId, item.title)}
+                        className="flex cursor-pointer hover:text-red-500"
+                      >
+                        <BiTrash size={20} />
+                        <span className="ml-2">REMOVE ITEM</span>
+                      </div>
+                      <div
+                        onClick={() => changeWishList(item.productId)}
+                        className={
+                          item?.like
+                            ? "ml-6 flex cursor-pointer text-red-500 "
+                            : "ml-6 flex cursor-pointer"
+                        }
+                      >
+                        <BsFillSuitHeartFill size={20} />
+                        <span className="ml-2">MOVE TO WISH LIST</span>
+                      </div>
+                    </div>
 
-                  <p className="sm:pb-4 text-base font-bold">
-                    ${item.price.toFixed(2)}
-                  </p>
+                    <p className="sm:pb-4 text-base font-bold">
+                      ${item.price.toFixed(2)}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <hr className="mb-2" />
-          </Fragment>
-        ))}
+              <hr className="mb-2" />
+            </Fragment>
+          ))
+        )}
       </div>
     </>
   );
--- src/pages/Product/ProductList.jsx
+++ src/pages/Product/ProductList.jsx
@@ -6,24 +6,40 @@ import {
   changeWishList,
   deleteProduct,
 } from "../../store/action/productAction";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProductList = () => {
   const dispatch = useDispatch();
   const [showAlert, setShowAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [id, setId] = useState();
+  const [title, setTitle] = useState();
 
   const hideAlert = () => {
     setShowAlert(false);
+    setId("");
+    setTitle("");
   };
 
   const onConfirm = () => {
     setShowAlert(false);
     dispatch(deleteProduct(id));
+    toast.success(`${title} deleted!`, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
   };
 
-  const onDelete = (id) => {
+  const onDelete = (id, title) => {
     setId(id);
+    setTitle(title);
     setShowAlert(true);
     setAlertMessage("Product data will be remove!");
   };
@@ -48,6 +64,20 @@ const ProductList = () => {
         >
           {alertMessage}
         </SweetAlert>
+      </div>
+      <div>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
       </div>
 
       <div className="flex justify-between l1148:mx-0 mx-28 my-8 mt-32 md:flex-col md:mx-4  ">
